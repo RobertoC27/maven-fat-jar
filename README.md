89a31668b9418b2fc192429ae04ad6c35942f0db
Roberto <chiroy_134@hotmail.com>
2020-02-25 19:26:45 -0600

    initial message

---
+++ README.md
@@ -0,0 +1,2 @@
+# A fat or uber-jar
+Based on the principle of containing all dependecies on a self-contained jar. This approach packs all dependecies into the output jar from the build process.
---
+++ src/main/java/gt/com/celera/tests/App.java
@@ -0,0 +1,18 @@
+package gt.com.celera.tests;
+import org.apache.logging.log4j.core.config.plugins.convert.EnumConverter;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
